712384cfd19aafd2c1235f4ba641c30f51037b61
Dylan Horgan <dyhorgan@gmail.com>
2022-04-12 12:43:25 -0400

    still cleaning up

--- src/components/tuiter/tuit-list/index.js
+++ src/components/tuiter/tuit-list/index.js
@@ -20,16 +20,7 @@ const TuitList = () => {
                                                 "time": "just now",
                                                 "propic": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/2449px-NASA_logo.svg.png"
                                               });
-    console.log("logging in tuitlist")
-    console.log(tuits)
-//     const findAllTuits = async () => {
-//       const tuits = await service.findAllTuits();
-//       dispatch({
-//         type: 'FIND_ALL_TUITS',
-//         tuits: tuits
-//       });
-//     }
-//     useEffect(findAllTuits, []);
+
 
  useEffect(() =>
    findAllTuits(dispatch),[dispatch]);
